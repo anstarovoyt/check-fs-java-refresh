44c25e9fc4c4cb6f88418d4ad8f82d378f5b8012
Andrew Starovoyt <astarovoyt@naumen.ru>
2014-09-30 20:58:59 +0400

    fix process waiting

--- test-java-fs/src/kkey/tst/Main.java
+++ test-java-fs/src/kkey/tst/Main.java
@@ -3,23 +3,27 @@ package kkey.tst;
 import java.io.File;
 import java.io.IOException;
 
-public class Main {
-    public static final int COUNTER = 2000;
+public class Main
+{
+	public static final int COUNTER = 2000;
 
-    public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, InterruptedException
+	{
 
-        File parent = new File("data-test");
-        parent.mkdir();
+		File parent = new File("data-test");
+		parent.mkdir();
 
-        for (int i = 0; i < COUNTER; i++) {
-            File dir = new File("data-test/forder-name-test-" + i);
-            dir.mkdir();
+		for (int i = 0; i < COUNTER; i++)
+		{
+			File dir = new File("data-test/forder-name-test-" + i);
+			dir.mkdir();
 
-            File dir2 = new File("data-test/forder-name-test-" + i);
-            Runtime.getRuntime().exec(new String[]{"touch", " test.data"}, new String[0], dir);
-            assert dir2.list().length > 0;
-        }
+			File dir2 = new File("data-test/forder-name-test-" + i);
+			Process exec = Runtime.getRuntime().exec(new String[]{"touch", " test.data"}, new String[0], dir);
+			exec.waitFor();
+			assert dir2.list().length > 0;
+		}
 
-        System.out.print("successful");
-    }
+		System.out.print("successful");
+	}
 }
