85732e406328796ce59a9c3fe79aa8b01846f919
Andrey Starovoyt <andrey.starovoyt@jetbrains.com>
2014-09-30 20:46:17 +0400

    add simple project

---
+++ test-java-fs/src/kkey/tst/Main.java
@@ -0,0 +1,25 @@
+package kkey.tst;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Main {
+    public static final int COUNTER = 2000;
+
+    public static void main(String[] args) throws IOException {
+
+        File parent = new File("data-test");
+        parent.mkdir();
+
+        for (int i = 0; i < COUNTER; i++) {
+            File dir = new File("data-test/forder-name-test-" + i);
+            dir.mkdir();
+
+            File dir2 = new File("data-test/forder-name-test-" + i);
+            Runtime.getRuntime().exec(new String[]{"touch", " test.data"}, new String[0], dir);
+            assert dir2.list().length > 0;
+        }
+
+        System.out.print("successful");
+    }
+}
